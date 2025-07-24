682a3b154c346490ae7ae1b3afe97fb36823613f
Kavia AI <production@kavia.ai>
2025-07-24 16:39:01 +0000

    CheckPoint - cgfb390c55

--- .init/.linter.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /home/kavia/workspace/code-generation/react-data-management-platform-82621-82630/frontend_app
-npm run build
-EXIT_CODE=$?
-if [ $EXIT_CODE -ne 0 ]; then
-   exit 1
-fi
-
